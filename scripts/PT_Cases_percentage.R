5a7c2d23e104c6142b90058cfd376ae2ad3b0aee
Vasco Alberto Filipe Ribeiro <vascoalbertofiliperibeiro@MBP-de-Vasco.home>
2020-03-31 21:39:16 +0100

    incongruence study of the variability of % of new cases detected

--- scripts/PT_Cases_percentage.R
+++ scripts/PT_Cases_percentage.R
@@ -17,3 +17,28 @@ percentageNewcasesPT <- gPercentage + stat_smooth(span = 3, col = rgb(0.8,0,0), 
 png(filename = "~/R/Covid_19/Covid_19/graphs/percentageNewcasesPT.png")
 percentageNewcasesPT
 dev.off()
+
+###############################################################
+# How the percentage of new cases detected varies day by day? #
+###############################################################
+
+
+Percentage.Data <- PTdataArranged$`Percentage cases`[3:length(PTdataArranged$`Percentage cases`)]
+Dates.Data <- PTdataArranged$dateRep[3:(length(PTdataArranged$dateRep)-1)]
+
+Dates.Data <- Dates.Data+1 ## correction to get real dates - we have a delay of one day 
+
+Wday.Data <- wday(Dates.Data, label = TRUE)
+
+diffPercentage.Data <- diff(Percentage.Data)
+
+TablePercentageDiff <- tibble(Dates = Dates.Data, WeekDay = Wday.Data, PercentageDiff = diffPercentage.Data)
+
+png(filename = "~/R/Covid_19/Covid_19/graphs/PercentageDiffStudy.png")
+
+par(mfrow = c(2,1), mar = c(4,1,2,1), mai = c(.8,1,0,.2))
+hist(TablePercentageDiff$PercentageDiff, breaks = 20, xlab = "% Difference", main = "")
+rug(TablePercentageDiff$PercentageDiff)
+plot(TablePercentageDiff$WeekDay, TablePercentageDiff$PercentageDiff, xlab = "weekday", ylab = "% Difference")
+
+dev.off()
